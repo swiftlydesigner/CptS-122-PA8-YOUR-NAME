c8c377916075e601611f44e36765ce4609ab4a89
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2024-10-16 19:00:06 -0700

    Create Skeleton Code of DbRepository

--- Development/macOS/PostgreSQL/PostgreSQL/DbRepository.cpp
+++ Development/macOS/PostgreSQL/PostgreSQL/DbRepository.cpp
@@ -6,3 +6,44 @@
 //
 
 #include "DbRepository.hpp"
+
+/// Construct a controller with a given filled out URL
+/// @Param fullURL The complete server URL for connecting to
+DbRepository::DbRepository(const string& fullURL) {
+    
+}
+
+/// Construct a controller with the given information. The URL will be built in the format: `postgresql://[user].[serverId]:[password]@[serverAddr]:[port]/[db-name]`
+/// @Param username User to connect as.
+/// @Param serverId ID of the serer to connect to.
+/// @Param password Password for the user for the specified`username`.
+/// @Param serverAddr The address of the server to connect to.
+/// @Param db The name of the database to connect to.
+DbRepository::DbRepository(const string& username, const string& serverId, const string& password, const string& serverAddr, const string& db) {
+    
+}
+
+/// Cleanup before destroying obejct
+DbRepository::~DbRepository() {
+    
+}
+
+/// Connect to the database
+bool DbRepository::connect() {
+    return false;
+}
+
+/// Check if the controller is connected to the database
+bool DbRepository::isConnected() const {
+    return false;
+}
+
+/// Return the name of the database we are connected to
+const string& DbRepository::getDatabaseName() const {
+    return nullptr;
+}
+
+/// Close the current database connection
+void close() {
+    
+}
