b22f49f45408f3fb5982a347f42d46dde381d524
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2024-10-17 08:40:36 -0700

    Add Enums for Reading Columns

---
+++ Development/macOS/PostgreSQL/PostgreSQL/Enums.hpp
@@ -0,0 +1,40 @@
+//
+//  Enums.hpp
+//  PostgreSQL
+//
+//  Created by Kyle Parker on 10/17/24.
+//
+
+#ifndef Enums_h
+#define Enums_h
+
+typedef enum : int {
+    REVIEW_ID, // text
+    URL, // text
+    PLACE_ID, // text
+    COUNTRY, // text
+    ADDRESS, // text
+    REVIEWER_NAME, // text
+    REVIEWS_BY_REVIEWER, // int8
+    PHOTOS_BY_REVIEWER, // text
+    REVIEWER_URL, // text
+    LOCAL_GUIDE, // bool
+    REVIEW_RATING, // int8
+    REVIEW, // text
+    REVIEW_DATE, // jsonb
+    NUMBER_OF_LIKES, // text
+    RESPONSE_OF_OWNER, // text
+    RESPONSE_DATE, // [NULLABLE] text
+    PHOTOS, // [NULLABLE] text
+    PLACE_NAME // text
+} GOOGLE_REVIEW_COLUMNS;
+
+typedef enum : int {
+    ID, // int8
+    USERNAME, // text
+    PASSWORD, // text
+    COUNTRIES, // text
+    SALT // uuid
+} GOOGLE_LOGIN;
+
+#endif /* Enums_h */
