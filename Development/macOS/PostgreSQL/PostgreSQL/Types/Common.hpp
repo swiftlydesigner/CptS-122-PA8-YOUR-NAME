2f17183b6a1dbb613f0369bfadaceade512346ea
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2024-10-17 22:51:53 -0700

    Add Common Functions

---
+++ Development/macOS/PostgreSQL/PostgreSQL/Types/Common.hpp
@@ -0,0 +1,31 @@
+//
+//  Common.hpp
+//  PostgreSQL
+//
+//  Created by main on 10/17/24.
+//
+
+#ifndef Common_hpp
+#define Common_hpp
+
+#include <iostream>
+
+#ifdef _WIN32
+#include <conio.h>
+#include <windows.h>
+#else
+#include <cstdlib>
+#include <termios.h>
+#include <unistd.h>
+#endif
+
+using std::cout;
+using std::endl;
+
+/// These functions will be defined, based on the OS.
+void clearScreen();
+
+void hideInput();
+void showInput();
+
+#endif /* Common_hpp */
