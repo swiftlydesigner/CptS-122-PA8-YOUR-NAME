3cdedb74de44eec9fdba6b92e60c732578ffca93
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2024-10-18 13:42:17 -0700

    Implement all of NodeData

--- Development/macOS/PostgreSQL/PostgreSQL/Tree/NodeData.cpp
+++ Development/macOS/PostgreSQL/PostgreSQL/Tree/NodeData.cpp
@@ -17,7 +17,11 @@ NodeData::NodeData(const string& businessName, const GoogleReviewEntity& entity)
 }
 
 /// Get the businessName of the node
-const string& businessName();
+const string& NodeData::businessName() {
+    return this->_businessName;
+}
 
 /// Get all entities
-const vector<GoogleReviewEntity>& getAllEntities();
+const vector<GoogleReviewEntity>& NodeData::getAllEntities() {
+    return this->_entities;
+}
