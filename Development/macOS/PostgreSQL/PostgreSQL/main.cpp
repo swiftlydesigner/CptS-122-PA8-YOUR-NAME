aeaffa75b952cde5320ec82e120422ed13801de0
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2024-10-17 22:55:44 -0700

    Implement Main

--- Development/macOS/PostgreSQL/PostgreSQL/main.cpp
+++ Development/macOS/PostgreSQL/PostgreSQL/main.cpp
@@ -2,13 +2,20 @@
 //  main.cpp
 //  PostgreSQL
 //
-//  Created by main on 10/3/24.
+//  Created by Kyle Parker on 10/3/24.
 //
 
-#include "DbRepository.hpp"
+#include "ReviewApp.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    ReviewApp* app = new ReviewApp;
+    
+    app->runApplication();
+    
+    delete app;
+    
     return 0;
 }
