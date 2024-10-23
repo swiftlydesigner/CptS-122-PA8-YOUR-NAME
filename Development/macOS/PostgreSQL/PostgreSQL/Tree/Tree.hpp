8b474bb864576c72330ebb4445e729b1b51fc4ff
swiftlydesigner <98251445+swiftlydesigner@users.noreply.github.com>
2024-10-23 11:19:20 -0700

    Make Tree Inherit From Base Class

--- Development/macOS/PostgreSQL/PostgreSQL/Tree/Tree.hpp
+++ Development/macOS/PostgreSQL/PostgreSQL/Tree/Tree.hpp
@@ -7,28 +7,18 @@
 
 /// Attention students: You should not modify any of the given code! Only build upon what you are given!
 
-/// TODO: Create methods as you need.
+/// TODO: Design Tree!
 
 #ifndef Tree_hpp
 #define Tree_hpp
 
-#include "TreeNode.hpp"
+#include "TreeBase.hpp"
 
-class Tree final {
+class Tree final : public TreeBase {
     /// TODO: Insert necessary members
     
-    bool insert(Node& node, const NodeData& newEntity);
-    
 public:
-    Tree();
     
-    Tree(const Tree& copy);
-    
-    Tree operator=(const Tree& rhs);
-    
-    ~Tree();
-    
-    bool insert(const NodeData& newEntity);
 };
 
 #endif /* Tree_hpp */
---
+++ Development/macOS/PostgreSQL/PostgreSQL/Tree/TreeBase.cpp
@@ -0,0 +1,8 @@
+//
+//  TreeBase.cpp
+//  PostgreSQL
+//
+//  Created by Kyle Parker on 10/23/24.
+//
+
+#include "TreeBase.hpp"
